9889616de9f216023bcc0491e6d282dcc50add55
AlastairM-E <alastair.mottram-epson@outlook.com>
2022-02-19 13:49:10 +0000

    performance site | 2022.02.19 | local environments have been added * Additions: LOcal environments added via dotenv and vite local stuff. * Use: Means that I should be able to access and edit environment variabels with netlify and finally add in the contact form functionality. Once the contact form is done, I will then have a site to deploy and test with. * WOrking : Yes.

--- functions/contactForm.ts
+++ functions/contactForm.ts
@@ -1,9 +1,11 @@
 import { Handler } from "@netlify/functions";
 
+
+
 const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: process.env.TEST_VAR }),
   };
 };
 
--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,15 @@
 import './tailwind.css';
 
-// fetch('/.netlify/functions/contactForm');
+console.log(import.meta.env);
+
+const contact = async () => {
+    const contactFormResponse = await fetch('/.netlify/functions/contactForm');
+    const contactFormJson = await contactFormResponse.json();
+
+    console.log({ contactFormJson });
+}
+
+contact();
 
 
-//  <FormLabel htmlFor="name">Name</FormLabel>
-// <Input
-//   type="text"
-//   name="name"
-//   value={name}
-//   onChange={($event: any) => setName($event.target.value)}
-//   borderWidth="2px"
-//   borderColor="black"
-//   _hover={{ borderColor: '#4299E1' }}
-// />
-// </FormControl>
-// <FormControl id="email" margin={2} width="95%" isRequired>
-// <FormLabel htmlFor="email">email</FormLabel>
-// <Input
-//   type="email"
-//   name="email"
-//   value={email}
-//   onChange={($event: any) => setEmail($event.target.value)}
-//   borderWidth="2px"
-//   borderColor="black"
-//   _hover={{ borderColor: '#4299E1' }}
-// />
-// </FormControl>
-// <FormControl id="message" margin={2} width="95%" isRequired>
-// <FormLabel htmlFor="message">Message</FormLabel>
-// <Textarea
-//   name="message"
-//   value={message}
-//   onChange={($event: any) => setMessage($event.target.value)}
-//   borderWidth="2px"
-//   borderColor="black"
-//   _hover={{ borderColor: '#4299E1' }}
-// />
-// </FormControl>
-// <Button
-// colorScheme="green"
-// type="submit"
-// isLoading={isLoading}
-// >
-// Submit
-// </Button> -->
 
-console.log('Hello world');
