518c7c2676572a33aba11fc80aae1eca9f9f6c12
AlastairM-E <alastair.mottram-epson@outlook.com>
2022-02-19 23:00:12 +0000

    performance site | 2022.02.19 | TYpescript strong checks, prevented me from putting in bad code and not using typescripts features enoguh. Make sure to mentally note to never turn off strict and noEmit error for typescript and instead fix the errors

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import './tailwind.css';
+const googleRecaptchaScript = document.getElementById('googleRecaptchaScript');
+const contactForm = document.getElementById('contactForm');
+googleRecaptchaScript?.setAttribute('src', `https://www.google.com/recaptcha/api.js?render=${import.meta.env.VITE_RECAPTCHA_SITE_KEY}`);
+contactForm?.addEventListener('submit', ($event) => {
+    $event.preventDefault();
+    grecaptcha.ready(async () => {
+        const nameField = document.getElementById('name');
+        const emailField = document.getElementById('email');
+        const messageTextarea = document.getElementById('message');
+        const token = await grecaptcha.execute(import.meta.env.VITE_RECAPTCHA_SITE_KEY, { action: 'submit' });
+        const contactFormApiResponse = await fetch('/.netlify/functions/contactForm', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: nameField.value,
+                email: emailField.value,
+                message: messageTextarea.value,
+                token
+            }),
+        });
+        console.log({ json: await contactFormApiResponse.json() });
+    });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './tailwind.css';
 
+type FormField = HTMLInputElement | null;
+
 const googleRecaptchaScript = document.getElementById('googleRecaptchaScript');
 const contactForm = document.getElementById('contactForm');
 googleRecaptchaScript?.setAttribute(
@@ -10,9 +12,9 @@ googleRecaptchaScript?.setAttribute(
 contactForm?.addEventListener('submit', ($event) => {
     $event.preventDefault();
     grecaptcha.ready(async () => {
-        const nameField: HTMLInputElement = document.getElementById('name');
-        const emailField: HTMLInputElement = document.getElementById('email');
-        const messageTextarea: HTMLInputElement = document.getElementById('message');
+        const nameField = document.getElementById('name') as FormField;
+        const emailField = document.getElementById('email') as FormField;
+        const messageTextarea = document.getElementById('message') as FormField;
         const token = await grecaptcha.execute(import.meta.env.VITE_RECAPTCHA_SITE_KEY, { action: 'submit' });
         const contactFormApiResponse = await fetch('/.netlify/functions/contactForm', {
             method: 'POST',
@@ -20,9 +22,9 @@ contactForm?.addEventListener('submit', ($event) => {
               'content-type': 'application/json',
             },
             body: JSON.stringify({
-              name: nameField.value, 
-              email: emailField.value, 
-              message: messageTextarea.value, 
+              name: nameField?.value, 
+              email: emailField?.value, 
+              message: messageTextarea?.value, 
               token
             }),
         });
