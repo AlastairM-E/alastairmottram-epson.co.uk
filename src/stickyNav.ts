4d0eedab41de96a816d1bf9fb53b95bb13a960b4
AlastairM-E <alastair.mottram-epson@outlook.com>
2022-03-09 23:10:33 +0000

    performance site | 2022.03.09 | sticky Nav completed, the case study of pull requests is nearly there. However, I need to work on the content,, add a contact form at the bottom and add a proper video at the top.

--- src/stickyNav.ts
+++ src/stickyNav.ts
@@ -1,11 +1,12 @@
 const stickyNav = document.getElementById('stickyNav');
 
 window.addEventListener('scroll', () => {
+    const MINOR_OFFSET_TO_PREVENT_UI_EFFECT = 30;
     if (!stickyNav) {
         return undefined;
     }
 
-    if (stickyNav?.offsetTop < window.pageYOffset)  {
+    if (stickyNav?.offsetTop < window.pageYOffset - MINOR_OFFSET_TO_PREVENT_UI_EFFECT)  {
         stickyNav?.classList.add('stickyNav');
     } else {
         stickyNav?.classList.remove('stickyNav');
