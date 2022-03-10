1f69be30f3351b6727a328d5effef71ca1f80403
AlastairM-E <alastair.mottram-epson@outlook.com>
2022-03-10 15:11:39 +0000

    perforamnce site | 2022.03.10 | Add in video for the perforamnce site and contact form, plus the footer has been fixed * Additions: site footer has been aded and the contact form as well as teh proper video. * Use: means that a case study is complete, so is giveable to Chris. * Working: Yes.

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# alastairm-e.co.uk - performance site
+# alastairmottram-epson.co.uk - performance site
 
 ## Purpose
-* Make a site more performant than old Gatsby.js static site
+
+- Make a site more performant than old Gatsby.js static site
 
 ## Results
-* 0.4 seconds faster by migrating to HTML, Tailwindcss & vanilla Typescript site by site from 541KB to 23KB.
+
+- 0.4 seconds faster by migrating to HTML, Tailwindcss & vanilla Typescript site by site from 541KB to 23KB.
 
 Check out [current site results](https://www.webpagetest.org/result/220227_AiDcC9_7N3/) and [old site results](https://www.webpagetest.org/result/220227_AiDcVW_7J4/).
 
 |                     | Current site (HTML, Tailwindcss, Typescript) | old site (Gatsby.js, Typescript) |
-|:------------------- |:-------------------------------------------- |:-------------------------------- |
+| :------------------ | :------------------------------------------- | :------------------------------- |
 | First load          | 1.3s                                         | 1.7s                             |
 | Total blocking time | 0s                                           | 1.4s                             |
 | App size            | 23KB                                         | 541KB                            |
-
--- functions/contactForm.ts
+++ functions/contactForm.ts
@@ -1,21 +1,20 @@
 import { Handler } from "@netlify/functions";
-import axios from 'axios';
-import sgMail from '@sendgrid/mail';
+import axios from "axios";
+import sgMail from "@sendgrid/mail";
 
 interface RecaptchaInterface {
   data: {
     success: boolean;
     score: number;
-  }
+  };
 }
 
 const handler: Handler = async ($event, context) => {
-
-  if ($event.httpMethod !== 'POST') {
+  if ($event.httpMethod !== "POST") {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Only POST queries" })
-    }
+      body: JSON.stringify({ message: "Only POST queries" }),
+    };
   }
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -23,9 +22,9 @@ const handler: Handler = async ($event, context) => {
   const PASSING_USER_SCORE = 0.7;
   const { name, email, message, token } = JSON.parse($event.body);
   const msg = {
-    to: 'alastair.me.opensource@gmail.com',
-    from: 'alastair.mottram-epson@outlook.com',
-    subject: 'alastairm-e.co.uk query',
+    to: "alastair.me.opensource@gmail.com",
+    from: "alastair.mottram-epson@outlook.com",
+    subject: "alastairmottram-epson.co.uk query",
     text: `
     Name: ${name},
     Email: ${email},
@@ -35,23 +34,26 @@ const handler: Handler = async ($event, context) => {
 
   try {
     const recapatcha: RecaptchaInterface = await axios.post(
-      'https://www.google.com/recaptcha/api/siteverify',
-      `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+      "https://www.google.com/recaptcha/api/siteverify",
+      `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
     );
 
-    if (recapatcha.data.success && recapatcha.data.score >= PASSING_USER_SCORE) {
+    if (
+      recapatcha.data.success &&
+      recapatcha.data.score >= PASSING_USER_SCORE
+    ) {
       await sgMail.send(msg);
 
       return {
         statusCode: 200,
-        body: JSON.stringify({ isEmailSent: true })
-      }
+        body: JSON.stringify({ isEmailSent: true }),
+      };
     }
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ isEmailSent: false })
-    }
+      body: JSON.stringify({ isEmailSent: false }),
+    };
   }
 };
 
