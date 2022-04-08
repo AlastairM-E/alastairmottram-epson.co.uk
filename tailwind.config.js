0d9d36b78304e15d2c99d8082a5ae3c0d37c3941
AlastairM-E <alastair.mottram-epson@outlook.com>
2022-04-08 23:59:52 +0100

    alastairmottram-epson.co.uk | 2022.04.08 | Added view cv section, round corners of the table, view on pull requests case study.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   content: [
     "./index.html",
+    "./case-study/**",
+    "./cv/**",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
